dceca3380fad2b0358233c64cb8bc73227ff157a
kaushik001-ind <kaushik.tib.001@gmail.com>
2026-04-01 14:06:55 +0530

    Update rg.tf

--- module/rg/rg.tf
+++ module/rg/rg.tf
@@ -1,1 +1,12 @@
-
+# -------------------------
+# modules/rg/rg.tf
+# -------------------------
+cat <<EOF > modules/rg/rg.tf
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+ 
+  tags = {
+    environment = "Dev"
+  }
+}
